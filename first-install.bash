916723025b411140ecdab084a55499fec0ddd0c7
DorjiTamang <146411325+DorjiTamang@users.noreply.github.com>
2023-10-03 11:40:59 +0600

    Add files via upload

---
+++ first-install.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#we firt want to update the pachages list
+#and upgrade the packages
+sudo apt update
+sudo apt upgrade
+#then we install some needed packages 
+sudo apt install terminator -y
+(cd /tmp && ([[ -d sexy-bash-prompt ]] || git clone --depth 1 --config core.autocrlf=false https://github.com/twolfson/sexy-bash-prompt) && cd sexy-bash-prompt && make install) && source ~/.bashrc
+
+sudo apt install arduino -y
+#add current user to dialogue group
+#please do not for to reboot
+sudo usermod -a -G dialout $USER
+
+# install gh
+# add apt respository 
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+reboot
